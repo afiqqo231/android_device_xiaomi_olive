e094400bc348418d1d8814b694939df4fa774af6
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2020-02-16 15:56:55 +0100

    olive: Adding some missing libs

--- device.mk
+++ device.mk
@@ -424,8 +424,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# USB
+# USB HAL
 PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0 \
     android.hardware.usb@1.0-service.basic
 
 # VNDK
